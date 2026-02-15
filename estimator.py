b89af1f5eb454ba3d73261a5ee77c93b3729be0c
Seunghun Paik <52731059+whitesoonguh@users.noreply.github.com>
2026-02-15 13:05:04 +0900

    Update Implementation

--- estimator.py
+++ estimator.py
@@ -19,6 +19,7 @@ def safe_log2_np(x):
     else:
         return np.log2(x)    
     
+    
 # This calculates the logarithm of surface area of a hyperspherical cap    
 def calculate_logarea(d, theta):
     return safe_log2_np(betainc((d-1)/2, 1/2, np.sin(theta * np.pi / 180) ** 2)) - 1
@@ -72,55 +73,52 @@ def plot_table(tab, sec_levels):
     plt.legend(fontsize = 20)
     plt.show()
     
-    
-# Estimator     
-        
-# Sample over the boundary of the given strip.
-@torch.no_grad()
-def eqangle(vec, angle, num = 100):
-    radius = math.sin(angle * math.pi / 180)
-    height = math.cos(angle * math.pi / 180)
-    
-    r = F.normalize(torch.randn((num, len(vec.T)), device = vec.device))
-    proj_r = r - vec.expand((num, len(vec.T))) * torch.matmul(vec, r.T).T / torch.matmul(vec, vec.T).item()
-    
-    return vec*height + radius * F.normalize(proj_r)
-    
-    
-    
-    
 ### ESTIMATOR ALGORTIHMS
-# Useful arithmetics
-def log_exp_add(a, b):
-    if a < b:
-        a, b = b, a
-    if b == -np.inf:
-        return a
+
+# Safe Functions
+def safe_log2(x: torch.Tensor) -> torch.Tensor:
+    # x can be on CPU or CUDA
+    neg_inf = torch.tensor(-torch.inf, device=x.device, dtype=x.dtype)
+    return torch.where(x == 0, neg_inf, torch.log2(x))
     
-    return a + safe_log2(1 + 2 ** (b-a))
-
-def log_exp_sub(a, b):
-    if a < b:
-        a, b = b, a
-        
-    if b == -np.inf:
-        return a
-    else:
-        return b + np.log2(2 ** (a-b) - 1)
-
-    
-# f/g functions    
-f_new = lambda d, theta:  safe_log2(betainc((d-1)/2, 1/2, np.sin(theta * np.pi / 180) ** 2)) - 1    
-g_new = lambda d, angle, eps: log_exp_sub(f_new(d, angle + eps), f_new(d, angle))
-
 def safe_add(x, y):
     if x == -torch.inf or y == -torch.inf:
         return -torch.inf
     else:
         return x + y
 
-# Transition Operator
-# Move vec towards vec2 by "to_angle"
+# Useful Arithmetics
+def log_exp_add(a, b):
+    if a < b:
+        a, b = b, a
+    if b == -np.inf:
+        return a
+    return a + safe_log2(1 + 2 ** (b-a))
+
+def log_exp_sub(a, b):
+    if a < b:
+        a, b = b, a
+    if b == -np.inf:
+        return a
+    else:
+        return b + safe_log2(2**(a-b) - 1)
+    
+# f/g functions    
+def f_new(d, theta):
+    return safe_log2_np(betainc((d-1)/2, 1/2, np.sin(theta * np.pi / 180) ** 2)) -1
+
+def g_new(d, theta_start, theta_end):
+    return log_exp_sub(f_new(d, theta_end), f_new(d, theta_start))
+
+# Vector Sampling & Transition
+@torch.no_grad()
+def eqangle(vec, angle, num = 100):
+    radius = math.sin(angle * math.pi / 180)
+    height = math.cos(angle * math.pi / 180)
+    r = F.normalize(torch.randn((num, len(vec.T)), device = vec.device))
+    proj_r = r - vec.expand((num, len(vec.T))) * torch.matmul(vec, r.T).T / torch.matmul(vec, vec.T).item()
+    return F.normalize(vec*height + radius * F.normalize(proj_r))
+    
 @torch.no_grad()
 def transition(vec, vec2, to_angle):
     radius = math.sin(to_angle * math.pi / 180)
@@ -128,100 +126,87 @@ def transition(vec, vec2, to_angle):
     
     proj_r = vec2 - vec.expand((vec2.size(0)), len(vec.T)) * torch.matmul(vec, vec2.T).T / torch.matmul(vec, vec.T).item()
     
-    return vec * height + radius * F.normalize(proj_r)
+    return F.normalize(vec * height + radius * F.normalize(proj_r))
         
-    
-# Reject Sampling for IM
-# TODO: Generalization
-def reject_sample(code, dim, alpha, angle, tg_num, N = 1<<12):
+        
+# Rejection Sampling
+@torch.no_grad()
+def reject_sample(BTP, t, tmp, angle, tg_num, N=1<<12):
     mem = []
     cnt = 0
     while cnt < tg_num:
-        x = eqangle(F.normalize(code), angle, N)
-        idx = ((ironmask_decode(x, alpha) == code).sum(dim=1) == dim)
+        x = eqangle(t, angle, N)
+        idx = BTP.auth(x, tmp)
         cnt += idx.sum()
         mem.append(x[idx, :])
-    return torch.cat(mem, dim = 0)[:tg_num]
+    return torch.cat(mem, dim = 0)[:tg_num]    
     
-
-# Estimator for IronMask
-from BTPs import BTP, GenC, ironmask_decode
-from tqdm.auto import tqdm
-
-def estimator_IM(dim, alpha, n_divs = 180, M = 1<<12, n_iter = 5):
-    divs = torch.linspace(0, 90, n_divs+1)[1:]
-#     divs = torch.linspace(1, 0, n_divs+1)[1:].acos() * (180/math.pi)
-    log_probs = []
-    for _ in range(n_iter):
-        ret_l = -torch.inf
-        ret_u = -torch.inf
-
-        code = GenC(dim, alpha).cuda()
-        prev = divs[0]
-        vec = eqangle(F.normalize(code), divs[0], M)
-        prob = ((ironmask_decode(vec, alpha) == code).sum(dim=1) == dim).sum() / M
-        log_prob = safe_log2(prob)
-
-
-        prev = divs[0]
+# Estimator
+@torch.no_grad()
+def estimator(BTP, device, n_divs=720, M=4096, n_iter=5, verbose = False):
+    divs = torch.linspace(0, 1, n_divs+1)[1:]
+    divs = divs.asin() * 180 / torch.pi
+    ret_l = -np.inf
+    ret_u = -np.inf
+    
+    if verbose: print("[LOG] Security Estimator Start...")    
+    for idx in range(n_iter):
+        # Initialize
+        t = F.normalize(torch.randn((1,512)))
+        tmp = BTP.enroll(t)    
+        prob_mem = []
+        t = t.to(device)
+        tmp = [z.to(device) for z in tmp]        
         
-        tmpp = [log_prob]
-
-
-        for i in tqdm(range(1, n_divs-1), "Security Estimator"):
-            curr = divs[i]
-            new = divs[i+1]
-            eps1 = curr-prev
-            eps2 = new-curr
-
-            tran_vec = transition(F.normalize(code), vec, curr)
-            tran_idx = ((ironmask_decode(tran_vec, alpha) == code).sum(dim=1) == dim)
-
+        prev = divs[0]                
+        vec = eqangle(t, divs[0], M)
+        ret = BTP.auth(vec, tmp)
+        prob = ret.sum() / M
+        log_prob = torch.tensor(0., device=device)
+        curr_prob_mem = [log_prob]
+            
+        for i in range(1, n_divs-1):  
+            # Step 1: Make Transition
+            tran_vec = transition(t, vec, divs[i])
+            
+            # Step 2: Compute Markov Prob            
+            tran_idx = BTP.auth(tran_vec, tmp)            
+            
             # Pr[E_i | E_i-1]
-            tran_prob = tran_idx.sum() / (vec.size(0) + 1e-12)
-
+            tran_prob = tran_idx.sum() / vec.size(0)
+            
+            # Step 3: Update log_prob and record it.
             log_prob = safe_add(log_prob, safe_log2(tran_prob))
+            curr_prob_mem.append(log_prob)
 
-
-    #         vec[tran_idx] = tran_vec[tran_idx]
-            if (~tran_idx).sum() > 0 and log_prob > -12:
+            # Step 4: If needed, do a rejection sampling for missing indices
+            if (~tran_idx).sum() > 0 and log_prob > -15:
                 vec[tran_idx] = tran_vec[tran_idx]
-                tmp = reject_sample(code, dim, alpha, curr, (~tran_idx).sum())
-                vec[~tran_idx] = reject_sample(code, dim, alpha, curr, (~tran_idx).sum())
+                vec[~tran_idx] = reject_sample(BTP, t, tmp, divs[i], (~tran_idx).sum(), N = M)
             else:
                 vec = tran_vec[tran_idx]
 
-            lower = g_new(512, prev, eps1)
-            upper = g_new(512, new, eps2)
-            lower = safe_add(lower, log_prob)
-            upper = safe_add(upper, log_prob)
-
-            ret_u = log_exp_add(ret_u, upper)
-            ret_l = log_exp_add(ret_l, lower)
-
-            prev = curr
-            tmpp.append(log_prob)
+        prob_mem.append(curr_prob_mem)
             
-        tmpp = torch.Tensor(tmpp)
-                        
-        log_probs.append(tmpp)
-                
-    log_probs = torch.cat(log_probs, dim = 0).reshape(n_iter, -1)
-    log_probs = torch.log2(torch.mean(2**log_probs, dim = 0))
-        
+    if verbose: print("[LOG] Prob Collection Done. Averaging Results...")
+    prob_mem = torch.FloatTensor(prob_mem)
+    prob_mem = torch.log2(torch.mean(2**prob_mem, dim = 0))
+    
     ret_l = -torch.inf
     ret_u = -torch.inf
-    prev = 0
+    full_divs = torch.cat([torch.tensor([0.0]), divs])
+
+    prev = divs[0]
     
-    for i, log_prob in enumerate(log_probs):
-        if i == 0:
+    for i, log_prob in enumerate(prob_mem):
+        if i ==0:
             continue
         curr = divs[i]
         new = divs[i+1]
         eps1 = curr-prev
         eps2 = new-curr        
-        lower = g_new(512, prev, eps1)
-        upper = g_new(512, new, eps2)
+        lower = g_new(512, prev, prev+eps1)
+        upper = g_new(512, new, new+eps2)
         lower = safe_add(lower, log_prob)
         upper = safe_add(upper, log_prob)
 
@@ -229,82 +214,6 @@ def estimator_IM(dim, alpha, n_divs = 180, M = 1<<12, n_iter = 5):
         ret_l = log_exp_add(ret_l, lower)       
         prev = curr
         
-    return (ret_l, ret_u)
-
-from BTPs import BTP, SigFeat
-
-# Estimator for SigFeat
-def reject_sample_SF(BTP, t, tmp, angle, tg_num, N = 1<<10):
-    mem = []
-    cnt = 0
-    while cnt < tg_num:
-        x = eqangle(F.normalize(t), angle, N)
-        c_x = BTP.auth(x, tmp)
-        idx = ((c_x == tmp[0]).sum(dim=1) == BTP.n)
-        cnt += idx.sum()
-        mem.append(x[idx, :])
-    return torch.cat(mem, dim = 0)[:tg_num]
-
-def estimator_SF(BTP, n_divs = 270, M = 1<<10):
-    divs = torch.linspace(0, 90, n_divs+1)[1:]
-    ret_l = -np.inf
-    ret_u = -np.inf
+    return (ret_l, ret_u)    
     
-    print("[LOG] GENERATE Target Template...")
-    t = F.normalize(torch.randn((1,512)))
-    _, tmp = BTP.enroll(t)
-    
-    prev = divs[0]
-    vec = eqangle(F.normalize(t), divs[0], M)
-    
-    print("[LOG] Initial Setup...")
-    c_x = BTP.auth(vec, tmp)
-    prob = ((tmp[0] == c_x).sum(dim = 1) == BTP.n).sum() / M
-    log_prob = safe_log2(prob)
-    
-    prev = divs[0]
-    
-    for i in tqdm(range(1, n_divs-1), "Security Estimator"):
-        curr = divs[i]
-        new = divs[i+1]
-        eps1 = curr-prev
-        eps2 = new-curr
-        
-        tran_vec = transition(F.normalize(t), vec, curr)
-        c_x = BTP.auth(tran_vec, tmp)
-        tran_idx = ((tmp[0] == c_x).sum(dim=1) == BTP.n)
-        
-        # Pr[E_i | E_i-1]
-        tran_prob = tran_idx.sum() / (vec.size(0) + 1e-12)
-        
-        log_prob = safe_add(log_prob, safe_log2(tran_prob))
-                
-        if (~tran_idx).sum() > 0 and log_prob > -12:
-            vec[tran_idx] = tran_vec[tran_idx]
-            vec[~tran_idx] = reject_sample_SF(BTP, t, tmp, curr, (~tran_idx).sum())
-        else:
-            vec = tran_vec[tran_idx]
-        
-        lower = g_new(512, prev, eps1)
-        upper = g_new(512, new, eps2)
-        lower = safe_add(lower, log_prob)
-        upper = safe_add(upper, log_prob)
-
-        ret_u = log_exp_add(ret_u, upper)
-        ret_l = log_exp_add(ret_l, lower)
-        
-        prev = curr
-        
-    return ret_l, ret_u
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
